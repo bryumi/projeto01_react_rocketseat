338f85f6ebd3acbed9e64b62f09de1fc40793359
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2023-01-30 15:35:03 -0300

    arrumando datas no js

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,36 @@ import styles from './App.module.css'
 import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60' ,
+      name: 'Bruna Yumi',
+      role: 'estudante',
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-01-29 21:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:'https://images.unsplash.com/photo-1517101724602-c257fe568157?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=412&q=60' ,
+      name: 'Rafael',
+      role: 'Analista',
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-01-30 10:00:00'),
+  },
+]
 export function App() {
 
   return (
@@ -15,8 +45,15 @@ export function App() {
         <Sidebar/>
       
         <main>
-          <Post />
-          <Post />
+          {posts.map (post => {
+            return (
+            <Post 
+              author= {post.author}
+              content = {post.content}
+              publishedAt = {post.publishedAt}
+            />
+            )
+          })}
         </main>
       </div>
       
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,40 @@
+import { format, formatDistanceToNow } from 'date-fns' 
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post(){
+export function Post({author, publishedAt, content}){
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR})
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { 
+        locale: ptBR,
+        addSuffix:true,
+    })
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60" />
+                    <Avatar src= { author.avatarUrl } />
                     <div className={styles.authorInfo}>
-                        <strong>Bruna Yumi</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title= "01 de Janeiro 2023 às 17h" dateTime='2023-01-27 17:00:00'>Publicado há 1h </time>
+                <time title= {publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow} </time>
             </header>
 
             <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
+                {content.map(line => {
+                    if(line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link'){
+                        return <p><a href='#'>{line.content}</a></p>
 
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-            <p><a href=''>jane.design/doctorcare</a></p>
-
-            <p>
-                <a href=''>#novoprojeto</a> {' '}
-                <a href=''>#nlw</a> {' '}
-                <a href=''>#rocketseat</a>
-            </p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
